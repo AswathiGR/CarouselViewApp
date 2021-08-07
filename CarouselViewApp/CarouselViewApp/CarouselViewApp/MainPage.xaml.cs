69d487dc8bba9d114377ecdbb2dc59e14450c659
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-08-07 20:22:08 +0530

    CarouselView in Xamarin forms

---
+++ CarouselViewApp/CarouselViewApp/CarouselViewApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+
+namespace CarouselViewApp
+{
+    public class MainPage
+    {
+       
+
+        private ObservableCollection<Class1> place;
+
+
+        // Learn more about making custom code visible in the Xamarin.Forms previewer
+        // by visiting https://aka.ms/xamarinforms-previewer
+        [DesignTimeVisible(false)]
+        public class MainPage : ContentPage
+    {
+        public class MainPage()
+        {
+   
+            place = new ObservableCollection<Class1>();
+            place.Add(new Class1 { Title = "Asian Paradise Flycatcher", ImagePath = "Asian-Paradise-Flycatcher.jpg" });
+            place.Add(new Class1 { Title = "Sarus Crane", ImagePath = "Sarus-Crane.jpg" });
+            place.Add(new Class1 { Title = "Himalayan Monal", ImagePath = "Himalayan-Monal.jpg" });
+           
+            //InitializeComponent();
+        }
+    }
+}
